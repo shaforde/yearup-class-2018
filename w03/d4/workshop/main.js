0586aa203e443fd173407a02e1662c5ed88e0b7a
stadmin <stadmin@nyc.yearup.org>
2018-11-08 16:09:09 -0500

    finished workshop

--- w03/d4/workshop/main.js
+++ w03/d4/workshop/main.js
@@ -1,3 +1,13 @@
 //This comes from data.js
-var gifs = window.data.data;
+let gifs = window.data.data;
 
+function randomCatGif(){
+  let boxElement = document.getElementById('cat');
+    let randomNumber = Math.floor(Math.random() * gifs.length);
+    boxElement.innerHTML = gifs[randomNumber];
+    boxElement.src=gifs[randomNumber].images.original.url;
+
+    console.log( gifs);
+  }
+  let button = document.querySelector('button');
+  button.addEventListener('click', randomCatGif);
