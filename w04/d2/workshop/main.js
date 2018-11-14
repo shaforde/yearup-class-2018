136364bb4886d2eea9474abb29502cb8925c85e8
stadmin <stadmin@nyc.yearup.org>
2018-11-14 16:13:37 -0500

    change first 2 pics

--- w04/d2/workshop/main.js
+++ w04/d2/workshop/main.js
@@ -1,1 +1,16 @@
 console.log('🚀');
+ axios.get("https://www.redbullshopus.com/products.json")
+  .then(insertImage)
+
+function insertImage(response){
+  console.log(response.data.products[0].images[0].src);
+   let img = document.getElementById("pic1");
+      img.src = response.data.products[0].images[0].src;
+}
+axios.get("https://www.redbullshopus.com/products.json")
+ .then(insertImage2)
+function insertImage2(response){
+  console.log(response.data.products[1].images[0].src);
+   let img = document.getElementById("pic2");
+      img.src = response.data.products[1].images[0].src;
+}
